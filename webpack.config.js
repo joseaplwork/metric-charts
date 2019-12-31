0fd892de80926f8f0021fcf87710fcfdfe0378ad
Jose Antonio Paredes Leon <joseparedes@iMac-de-Jose.home>
2019-12-31 04:03:37 +0100

    Improve webpack configuration to boost app loading time

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const StyleExtHtmlWebpackPlugin = require('style-ext-html-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 const TerserJSPlugin = require('terser-webpack-plugin');
 
+const bundleName = 'bundle.js';
 const devConfig = {};
 
 if (process.env.NODE_ENV === 'development') {
@@ -19,8 +21,8 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    chunkFilename: '[name].bundle.js',
-    filename: 'bundle.js',
+    chunkFilename: `[name].${bundleName}`,
+    filename: bundleName,
   },
   resolve: {
     alias: {
@@ -75,6 +77,11 @@ module.exports = {
         useShortDoctype: true,
       },
       template: './client/index.html',
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      inline: /^(.*?(\bvendors\b)[^$]*)$/,
+      async: bundleName,
+      preload: bundleName,
     }),
     new MiniCssExtractPlugin(),
     new StyleExtHtmlWebpackPlugin(),
